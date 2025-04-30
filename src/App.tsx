8abc182eb40af84ddb9a93295e0f9d0fa1a3773c
prosper90 <Onyekosorprosper@gmail.com>
2025-05-01 00:33:37 +0100

    made new changes again

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
-import { BrowserRouter as Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import ProtectedRoute from './routes/ProtectedRoute'
 import Team from './pages/Team'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
